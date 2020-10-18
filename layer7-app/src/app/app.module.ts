2da5c9d6643daa290be02cf3367352dbd035149b
Adam Swanson <adam.swanson.98@gmail.com>
2020-10-18 17:04:09 -0400

    Add bootstrap

--- layer7-app/src/app/app.module.ts
+++ layer7-app/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { RouterModule, Routes } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 const ROUTES = [
   { path: '', component: HomeComponent }
@@ -16,7 +17,8 @@ const ROUTES = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
